0778f91611af48621b42455c629461385732ed3f
EMeMSoft s.r.o. <miroslav.mamrak@seznam.cz>
2025-01-15 13:37:51 +0100

    fix: fix various little errors in CustomerEntity and CustomerSaveRequest

--- src/main/kotlin/cz/ememsoft/minibank/api/dto/request/CustomerSaveRequest.kt
+++ src/main/kotlin/cz/ememsoft/minibank/api/dto/request/CustomerSaveRequest.kt
@@ -1,18 +1,17 @@
 package cz.ememsoft.minibank.api.dto.request
 
 import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Pattern
-import jakarta.validation.constraints.Positive
-
 
 data class CustomerSaveRequest(
-    @Positive
-    val id: Long,
     @NotNull
+    @NotBlank
     val firstName: String,
     @NotNull
-    val surname: String,
+    @NotBlank
+    val lastName: String,
     @Email(message = "Invalid email")
     val email: String,
     @Pattern(
--- src/main/kotlin/cz/ememsoft/minibank/entity/CustomerEntity.kt
+++ src/main/kotlin/cz/ememsoft/minibank/entity/CustomerEntity.kt
@@ -16,15 +16,15 @@ class CustomerEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "customer_id")
-    val id: Long = 0,
+    private val id: Long = 0,
 
     @NotNull
     @Column(name = "first_name", nullable = false)
     val firstName: String,
 
     @NotNull
-    @Column(name = "surname", nullable = false)
-    val surname: String,
+    @Column(name = "last_name", nullable = false)
+    val lastName: String,
 
     @Email(message = "Invalid email")
     @Column(name = "email", unique = true, nullable = false)
@@ -39,4 +39,28 @@ class CustomerEntity(
 
     @Column(name = "address", nullable = false)
     val address: String,
-) {}
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is CustomerEntity) return false
+
+        if (id != other.id) return false
+        if (firstName != other.firstName) return false
+        if (lastName != other.lastName) return false
+        if (email != other.email) return false
+        if (phone != other.phone) return false
+        if (address != other.address) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + firstName.hashCode()
+        result = 31 * result + lastName.hashCode()
+        result = 31 * result + email.hashCode()
+        result = 31 * result + phone.hashCode()
+        result = 31 * result + address.hashCode()
+        return result
+    }
+}
