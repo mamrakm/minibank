11ddc7f2180ee2c2480b9fa5469f942417a4b91c
EMeMSoft s.r.o. <miroslav.mamrak@seznam.cz>
2025-01-19 19:12:05 +0100

    build: remove native config files

--- src/test/kotlin/cz/ememsoft/minibank/MinibankApplicationTests.kt
+++ src/test/kotlin/cz/ememsoft/minibank/MinibankApplicationTests.kt
@@ -1,6 +1,6 @@
 package cz.ememsoft.minibank
 
-import cz.ememsoft.minibank.repository.CustomerRepository
+import cz.ememsoft.minibank.repository.ClientRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -19,14 +19,50 @@ import org.testcontainers.junit.jupiter.Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 class MinibankApplicationTests(
-    @Autowired private val customerRepository: CustomerRepository,
-    @Autowired private var restTemplateBuilder: RestTemplateBuilder,
+    @Autowired private val clientRepository: ClientRepository,
+    @Autowired private val restTemplateBuilder: RestTemplateBuilder,
 ) {
     @LocalServerPort
     private var port: Int = 0
 
+//    @BeforeEach
+//    fun setup() {
+//        // Create a RestTemplate instance from RestTemplateBuilder
+//        val restTemplate: RestTemplate by lazy {
+//            restTemplateBuilder.build()
+//        }
+//        sendInitialClientData()
+//    }
+//
+//    private fun sendInitialClientData() {
+//        val customerJson = """
+//            {
+//                "firstName": "Johnny",
+//                "lastName": "Silverhand",
+//                "email": "johnny.silverhand@nightcity.com",
+//                "phone": "+123456789",
+//                "address": "Mega Building 10, Litlle China, Watson, Night City"
+//            }
+//        """.trimIndent()
+//
+//        val headers = HttpHeaders().apply {
+//            contentType = MediaType.APPLICATION_JSON
+//        }
+//
+//        val request = HttpEntity(customerJson, headers)
+//
+//        val response = restTemplate.exchange(
+//            "http://localhost:$port/customers/save", // Ensure the correct endpoint
+//            HttpMethod.POST,
+//            request,
+//            String::class.java
+//        )
+//
+//        assertThat(response).isEqualTo(201) // Verify the data is saved
+//    }
+
     @Test
-    fun `save a customer to DB`() {
+    fun `save a client to DB`() {
         val customerJson = """
             {
                 "firstName": "Johnny",
@@ -37,31 +73,27 @@ class MinibankApplicationTests(
             }
         """.trimIndent()
 
-        // Create a RestTemplate instance from RestTemplateBuilder
         val restTemplate: RestTemplate by lazy {
             restTemplateBuilder.build()
         }
 
-        // Set up headers
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_JSON
 
-        // Create HTTP entity with headers and body
         val request = HttpEntity(customerJson, headers)
 
         restTemplate.exchange(
             "http://localhost:$port/customers/save", HttpMethod.POST, request, String::class.java
         )
 
-        val result = customerRepository.findAll()
+        val result = clientRepository.findAll()
         result.forEach { println(it) }
 
-        assertThat(result)
-            .hasSize(1)
-        assertThat(result[0].firstName).isEqualTo("Johnny")
-        assertThat(result[0].lastName).isEqualTo("Silverhand")
-        assertThat(result[0].email).isEqualTo("johnny.silverhand@nightcity.com")
-        assertThat(result[0].phone).isEqualTo("+123456789")
-        assertThat(result[0].address).isEqualTo("Mega Building 10, Litlle China, Watson, Night City")
+        assertThat(result).hasSize(1)
+        assertThat(result[0]?.firstName).isEqualTo("Johnny")
+        assertThat(result[0]?.lastName).isEqualTo("Silverhand")
+        assertThat(result[0]?.email).isEqualTo("johnny.silverhand@nightcity.com")
+        assertThat(result[0]?.phone).isEqualTo("+123456789")
+        assertThat(result[0]?.address).isEqualTo("Mega Building 10, Litlle China, Watson, Night City")
     }
 }
