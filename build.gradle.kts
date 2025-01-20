c6e920c153c8c9d72899a7a37fc9866215db2717
Miroslav Mamrák <miroslav.mamrak@pumpitup.cz>
2025-01-20 06:29:40 +0100

    build: remove native image support

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.hibernate.orm") version "6.1.1.Final"
-    id("org.graalvm.buildtools.native") version "0.10.4"
     kotlin("plugin.jpa") version "1.9.25"
     kotlin("kapt") version "1.9.25"
     id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
@@ -84,33 +83,6 @@ allOpen {
     annotation("jakarta.persistence.Entity")
     annotation("jakarta.persistence.MappedSuperclass")
     annotation("jakarta.persistence.Embeddable")
-}
-
-graalvmNative {
-    metadataRepository {
-        version = "0.3.15"
-//        enabled = false
-    }
-    agent {
-        defaultMode = "standard" // Default agent mode if one isn't specified using `-Pagent=mode_name`
-        enabled = true // Enables the agent
-        builtinCallerFilter = true
-        builtinHeuristicFilter = true
-        enableExperimentalPredefinedClasses = true
-        enableExperimentalUnsafeAllocationTracing = true
-        trackReflectionMetadata = true
-    }
-    binaries {
-        all {
-            buildArgs.add("-H:+UnlockExperimentalVMOptions")
-            buildArgs.add("-H:+AllowIncompleteClasspath")
-            buildArgs.add("-H:+SupportPredefinedClasses")
-            buildArgs.add("-H:+BuildReport")
-
-            resources.autodetect()
-            quickBuild = false
-        }
-    }
 }
 
 tasks.withType<Test> {
