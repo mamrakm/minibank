adfdea0e4bc6f9035ad01564e1547efe423cb1e3
Miroslav Mamrák <miroslav.mamrak@pumpitup.cz>
2025-01-20 01:40:40 +0100

    feat: annotate AccountEntity and create id

--- src/main/kotlin/cz/ememsoft/minibank/entity/AccountEntity.kt
+++ src/main/kotlin/cz/ememsoft/minibank/entity/AccountEntity.kt
@@ -1,4 +1,17 @@
 package cz.ememsoft.minibank.entity
 
-class AccountEntity {
-}
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "account", schema = "bank")
+class AccountEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id")
+    val id: Long,
+)
