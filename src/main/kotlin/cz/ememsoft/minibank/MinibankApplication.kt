336741c6ba9ef3dcfbdd0181d26cc6d501ee47f9
EMeMSoft s.r.o <miroslav.mamrak@seznam.cz>
2025-01-20 17:42:19 +0100

    feat: add async support annotation to application

--- src/main/kotlin/cz/ememsoft/minibank/MinibankApplication.kt
+++ src/main/kotlin/cz/ememsoft/minibank/MinibankApplication.kt
@@ -2,7 +2,9 @@ package cz.ememsoft.minibank
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
+@EnableAsync
 @SpringBootApplication
 class MinibankApplication
 
