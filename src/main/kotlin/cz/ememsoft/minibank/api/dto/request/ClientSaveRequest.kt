627256ff8d341023703e5801e1f49ab5226cdf16
EMeMSoft s.r.o <miroslav.mamrak@seznam.cz>
2025-01-30 13:22:56 +0100

    feat: add DTOs for new requests and responses

---
+++ src/main/kotlin/cz/ememsoft/minibank/api/dto/request/ClientSaveRequest.kt
@@ -0,0 +1,27 @@
+package cz.ememsoft.minibank.api.dto.request
+
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Pattern
+
+data class ClientSaveRequest(
+    @NotNull
+    @NotBlank
+    val firstName: String,
+    @NotNull
+    @NotBlank
+    val lastName: String,
+    @Email(message = "Invalid email")
+    val email: String,
+    @Pattern(
+        regexp = "^\\+?[1-9]\\d{1,14}\$",
+        message = "Invalid phone number"
+    )
+    val phone: String,
+    val address: String,
+) {
+    override fun toString(): String {
+        return "CustomerSaveRequest(firstName='$firstName', lastName='$lastName', email='$email', phone='$phone', address='$address')"
+    }
+}
