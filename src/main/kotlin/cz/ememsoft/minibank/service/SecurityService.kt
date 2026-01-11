63469a144693225421963ab52e8f5206ed006471
Miroslav Mamrák <mamrakm@users.noreply.github.com>
2026-01-11 14:07:27 +0100

    fix: avoid duplicate transaction failure handling

--- src/main/kotlin/cz/ememsoft/minibank/service/SecurityService.kt
+++ src/main/kotlin/cz/ememsoft/minibank/service/SecurityService.kt
@@ -24,8 +24,7 @@ class SecurityService(
      */
     fun getAuthenticatedClientId(): Mono<Long> {
         return ReactiveSecurityContextHolder.getContext()
-            .mapNotNull { it.authentication?.principal as? Jwt }
-            .map { it.subject }
+            .flatMap { Mono.justOrEmpty((it.authentication?.principal as? Jwt)?.subject) }
             .switchIfEmpty(Mono.error(ClientUnauthorizedException("No authenticated user found")))
             .flatMap { keycloakUserId ->
                 clientRepository.findByKeycloakUserId(keycloakUserId)
